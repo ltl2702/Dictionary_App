e732b3b8657956075d9e9074e407dff37ded0475
Lương Thị Linh <luongthilinh2702@gmail.com>
2023-10-01 17:29:56 +0700

    update

---
+++ Dictionary 1.0/src/main/java/translator/Dictionary.java
@@ -0,0 +1,15 @@
+package translator;
+
+import java.util.TreeMap;
+
+public class Dictionary {
+    public final TreeMap<String, String> wordList = new TreeMap<>();
+
+    public TreeMap<String, String> getWords() {
+        return wordList;
+    }
+
+    public void insert(Word word) {
+        wordList.put(word.getWord_source(), word.getWord_target());
+    }
+}
