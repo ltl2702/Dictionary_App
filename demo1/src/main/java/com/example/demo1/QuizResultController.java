42c795fce1c315a1f0eb33e5b070a3abb48d47f6
vtq0611 <quynhvutu0611@gmail.com>
2023-11-24 23:56:42 +0700

    folder game quiz rieng

---
+++ demo1/src/main/java/com/example/demo1/QuizResultController.java
@@ -0,0 +1,32 @@
+package com.example.demo1;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class QuizResultController implements Initializable {
+    @FXML
+    Label yourScore, correct, wrong;
+    @FXML
+    Button exit, replay;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        yourScore.setText("Your score: " + QuestionController.correct + "/10");
+        correct.setText("Correct answers: " + String.valueOf(QuestionController.correct));
+        wrong.setText("Incorrect answers: " + String.valueOf(QuestionController.wrong));
+    }
+
+    public void exitSetOnAction() {
+
+    }
+
+    //public void replay() {
+        //QuestionController.loadQuestions();
+    //}
+
+}
