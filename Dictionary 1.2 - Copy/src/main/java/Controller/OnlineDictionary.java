8ff0839ae04fc5111b91950b7d0dcb616b23d8ff
nttt2004 <124849254+nttt2004@users.noreply.github.com>
2023-11-27 15:43:18 +0700

    onlineDictionary

---
+++ Dictionary 1.2 - Copy/src/main/java/Controller/OnlineDictionary.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+
+public class OnlineDictionary {
+
+    @FXML
+    private Hyperlink CamHyperLink;
+
+    @FXML
+    private Hyperlink OxfordHyperLink;
+
+    @FXML
+    private JFXButton okButton;
+
+    @FXML
+    private Hyperlink uetHyperLink;
+
+    @FXML
+    void handleHyperLinkClick(ActionEvent event) {
+
+    }
+
+}
