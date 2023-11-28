8913067c54cc2903e301b1b8ae01b9580ba52f66
Lương Thị Linh <luongthilinh2702@gmail.com>
2023-11-28 11:45:44 +0700

    sua defitinion

--- Dictionary 1.2 - Copy/src/main/java/Controller/Home.java
+++ Dictionary 1.2 - Copy/src/main/java/Controller/Home.java
@@ -104,6 +104,7 @@ public class Home implements Initializable {
         MenuClose.setOnMouseClicked(event -> animateMenu(-400, animationDuration));
 
         webEngine = webView.getEngine();
+        webView.setDisable(true);
         searchField.textProperty().addListener((observable, oldValue, newValue) -> {
             performSearch(newValue);
         });
